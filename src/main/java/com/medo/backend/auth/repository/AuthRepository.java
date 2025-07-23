2ae360916c2ad5b53bd6a4b4e086fc7e87e8cc3f
mohamed <iaazi.mohamed.1998@gmail.com>
2025-07-23 10:49:41 +0100

    fix

--- src/main/java/com/medo/backend/auth/repository/AuthRepository.java
+++ src/main/java/com/medo/backend/auth/repository/AuthRepository.java
@@ -2,9 +2,11 @@ package com.medo.backend.auth.repository;
 
 import com.medo.backend.user.model.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface AuthRepository extends CrudRepository<User ,Long> {
 
     Optional<User> findUserByName(String username);
